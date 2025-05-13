625642fe00e79150a6f42380b5be67c7dd4bef78
Ahmed Hashem <83191231+he4a00@users.noreply.github.com>
2025-05-13 16:02:25 +0300

    Update problem1.js

--- src/problem1.js
+++ src/problem1.js
@@ -1,20 +1,3 @@
-// function checkOnes(input) {
-//   let counter = 0; // this is a counter, it increases each time we see a 1, and it stay at its current value if we see 0
-
-//   // here we map over the input to see eact character on its own, like if it is 001 we se it as 0 => 0 => 1
-//   for (const char of input) {
-//     // here we check that if the single char that our loop at is equal to 1, if it is then we increase the cunter by 1 then see if it divisible by 3
-//     if (char === "1") counter = (counter + 1) % 3;
-//     else if (char !== "0") return false; // here we reject each character that is not 0 and 1 cuz this is a binary problem
-//   }
-//   return counter === 0; // so after we reset the counter to 0 after it reach 3
-// }
-
-// some test cases
-// console.log(checkOnes("110"));
-// console.log(checkOnes("111"));
-// console.log(checkOnes("10101"));
-
 class DFA {
   constructor() {
     // here we initialize the dfa
